bec82753de1c8a929322d057b05576fc95a8ec00
jsk900 <jsk900@yahoo.co.uk>
2019-09-01 10:20:48 +0200

    Spotify with infinite scrolling

--- main.js
+++ main.js
@@ -2,25 +2,31 @@ window.onload = () => start();
 
 //Globals
 const url = 'https://elegant-croissant.glitch.me/spotify';
-let artist;
-let type = 'album';
+let artist = 'prince';
+let type = 'artist';
+let urlLoad;
+let search;
+let nextPage;
 
 //Get DOM Elements
 const input = document.querySelector('input[type="text"]');
-const button = document.querySelector('button');
+const select = document.querySelector('select');
+const go = document.querySelector('button');
 const section = document.querySelector('section');
+const footer = document.querySelector('footer');
 
 const start = () => {
   input.focus();
+  urlLoad = `${url}?q=${artist}&type=${type}`;
+  dataLoad(urlLoad);
 };
 
 //Get data from api
-const dataLoad = () => {
-  let urlLoad = `${url}?q=${artist}&type=${type}`;
-  fetch(urlLoad)
+const dataLoad = url => {
+  fetch(url)
     .then(checkStatus)
     .then(response => response.json())
-    .then(response => response.albums.items.map(art => show(art)))
+    .then(response => show(response))
     .catch(error => errorHandler(error));
 };
 
@@ -34,22 +40,30 @@ const checkStatus = response => {
 };
 
 //Show data
-const show = art => {
-  let figure = document.createElement('figure');
-  let imagePlaceholder = document.createElement('img');
-  let figcaption = document.createElement('figcaption');
-  imagePlaceholder.src = art.images[0].url;
+const show = response => {
+  search = response.artists || response.albums;
+  nextPage = search.next;
+  search.items.map(art => {
+    let figure = document.createElement('figure');
+    let imagePlaceholder = document.createElement('img');
+    let figcaption = document.createElement('figcaption');
+    if (art.images.length !== 0) {
+      imagePlaceholder.src = art.images[0].url;
+    } else {
+      imagePlaceholder.src = './images/noImage.png';
+    }
 
-  if (art.name.length > 50) {
-    let newTitle = art.name.substring(0, 30);
-    figcaption.innerHTML = newTitle;
-  } else {
-    figcaption.innerHTML = art.name;
-  }
+    if (art.name.length > 50) {
+      let newTitle = art.name.substring(0, 30);
+      figcaption.innerHTML = newTitle;
+    } else {
+      figcaption.innerHTML = art.name;
+    }
 
-  figure.appendChild(imagePlaceholder);
-  figure.appendChild(figcaption);
-  section.appendChild(figure);
+    figure.appendChild(imagePlaceholder);
+    figure.appendChild(figcaption);
+    section.appendChild(figure);
+  });
 };
 
 const deleteList = () => {
@@ -60,17 +74,44 @@ const deleteList = () => {
   }
 };
 
+const infinite = () => {
+  if (nextPage) {
+    urlLoad = nextPage.replace(
+      'https://api.spotify.com/v1/search',
+      'https://elegant-croissant.glitch.me/spotify'
+    );
+    dataLoad(urlLoad);
+  }
+};
+
 //Handle errors
 const errorHandler = error => {
-  console.log('error');
+  console.log(error);
 };
 
 //Listeners
-button.addEventListener('click', e => {
+select.addEventListener('click', e => {
+  type = e.target.value;
+});
+
+go.addEventListener('click', e => {
   e.preventDefault();
   artist = input.value;
+  urlLoad = `${url}?q=${artist}&type=${type}`;
   input.value = '';
   input.focus();
   deleteList();
-  dataLoad();
+  dataLoad(urlLoad);
+});
+
+//Very neat code to handle infinite scrolling
+document.addEventListener('DOMContentLoaded', () => {
+  let options = {
+    root: null,
+    rootMargins: '0px',
+    threshold: 0.2
+  };
+
+  const observer = new IntersectionObserver(infinite, options);
+  observer.observe(footer);
 });
